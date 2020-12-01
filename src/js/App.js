327dcc1c96714a48bd6dba167ccaa99dcaebeae4
Michael Ron <mduenes2@islander.tamucc.edu>
2020-11-30 22:13:24 -0600

    added CSS grid to be able to display additional information

--- src/js/App.js
+++ src/js/App.js
@@ -1,26 +1,65 @@
-import profPhoto from '../MichaelDuenes.jpg';
+import React, {Component} from "react";
+
 import '../css/App.css';
 import '../css/text.css';
 import '../css/photo.css'
-import {Box} from "@material-ui/core";
-import Grid from "@material-ui/core/Grid";
+import profPhoto from '../MichaelDuenes.jpg';
 
-function App() {
-  return (
-    <div className="App">
-    <header className="App-header">
-        <p className="title">
-            Hello, my name is Michael Duenes
-        </p>
-            <Grid container spacing={10} justify="center">
-                <img src={profPhoto} className="profPhoto" alt="this is my photo"/>
-                <p className="about-me-title">
-                    About me
+
+import {Box, createMuiTheme, MuiThemeProvider, Grid, Button} from "@material-ui/core";
+import {aboutMe} from "./const";
+
+const theme = createMuiTheme({
+    palette:{
+        primary:{
+            main:'#86c232'
+        },
+    },
+});
+
+class App extends Component{
+
+  render() {
+      return (
+        <div className="App">
+            <header className="App-header">
+                <p className="title">
+                    Hello, my name is Michael Duenes
                 </p>
-            </Grid>
-    </header>
-    </div>
-  );
+
+                <Box className="about-me-container">
+
+                    <p className="about-me-title">
+                        About me
+                    </p>
+
+                    <p className="about-me-text">
+                        {aboutMe}
+                    </p>
+
+                    <img src={profPhoto} className="profPhoto" alt="" />
+                </Box>
+
+                <div className="grid-container">
+                    <div className="grid-item">
+                        <p className = "sub-title">
+                            Experience
+                        </p>
+                    </div>
+                    <div className="grid-item">
+                        <p className="sub-title">
+                            pepe
+                        </p>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+
+            </header>
+        </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/js/const.js
@@ -0,0 +1,4 @@
+export const aboutMe = "My name is Michael Duenes and I am a 4th year Computer Science major currently at Texas A&M University - Corpus Christi"+
+    ", with a minor in Mathematics. I aspire to become a full-stack developer but,"+
+    " I have taken a liking to front-end development during my time at USAA as a Technical Staff Intern."
+export const experience = ""
